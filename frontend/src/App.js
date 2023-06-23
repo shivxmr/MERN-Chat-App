b025c0abc365bd60810b38c1411873d5aa3b93aa
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-06-24 00:41:20 +0530

    added frontend and connected chatpage with backend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="/" component={HomePage} exact />
+      {/* We wrote exact above because path / exists in /chats also so when you go to /chats then homepage also comes because /chats contains / also so homepage also renders */}
+
+      <Route path="/chats" component={ChatPage} />
+    </div>
+  );
+}
+
+export default App;
